36d594be98bc9e6232658ab885e5a63a459c4a6e
Nisar Khan <khannisarnk0786@gmail.com>
2025-07-21 08:59:42 +0530

    fix error

---
+++ backend/types/fallback.d.ts
@@ -0,0 +1,6 @@
+declare module 'passport-google-oauth20';
+declare module 'passport-facebook';
+declare module 'nodemailer';
+declare module 'node-cron';
+declare module 'jsonwebtoken';
+declare module 'bcryptjs';
